3fe9bba8b36db962d295c41ac5641872709b17ca
Jundullah H. R <jundihanif77@gmail.com>
2021-10-18 23:52:11 +0700

    add three.js

---
+++ main.js
@@ -0,0 +1,6 @@
+import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.min.js"
+
+
+// BOTTOM IMPORT
+// Supaya bisa digunakan di console browser
+window.THREE = THREE

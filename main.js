e54180a0e963210fb3b47796876f834a8759dbba
Jundullah H. R <jundihanif77@gmail.com>
2021-10-19 00:22:07 +0700

    add event for init

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.min.js"
+// var THREE = {}
+var TowerBlock = {
+    renderArea: document.getElementById('render-area'),
+}
 
+function initTowerBlock() {
+    console.log('hi')
+}
 
 // BOTTOM IMPORT
 // Supaya bisa digunakan di console browser
 window.THREE = THREE
+window.TowerBlock = TowerBlock
+window.initTowerBlock = initTowerBlock
+
+// ADD EVENT
+window.addEventListener('load', initTowerBlock)

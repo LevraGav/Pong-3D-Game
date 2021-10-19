97c62733ca76e8ea9aec8cca49ed5894e08312b2
7farema <7farema@gmail.com>
2021-10-19 09:55:40 +0700

    ganti warna

--- pong3d/Scripts/game.js
+++ pong3d/Scripts/game.js
@@ -111,19 +111,19 @@ function createScene()
 	var planeMaterial =
 	  new THREE.MeshLambertMaterial(
 		{
-		  color: 0x4BD121
+		  color: 0x54ff68
 		});
 	// create the table's material
 	var tableMaterial =
 	  new THREE.MeshLambertMaterial(
 		{
-		  color: 0x111111
+		  color: 0x54ff68
 		});
 	// create the pillar's material
 	var pillarMaterial =
 	  new THREE.MeshLambertMaterial(
 		{
-		  color: 0x534d0d
+		  color: 0x6054ff
 		});
 	// create the ground's material
 	var groundMaterial =
